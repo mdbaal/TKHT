599aade4497c91872391cd1cbac46a53ce1e4ae8
mirco <mircobaalmans@live.nl>
2020-02-24 11:56:38 +0100

    Added gamemaster scriptableobject

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public Commands commands;
+
+    string action = "";
+    string target = "";
+
+    public void sendCommand(List<string> words)
+    {
+        action = words[0];
+        target = words[1];
+    }
+
+    private void checkCommand()
+    {
+        if (commands.checkCommand(action))
+        {
+            doAction();
+            return;
+        }
+    }
+
+    private void doAction()
+    {
+        switch(action){
+            case "go":
+
+                break;
+
+            
+        }
+    }
+
+}
