8e93a8de0cb350619d10805df6723e3dd2231501
mirco <mircobaalmans@live.nl>
2020-02-24 11:55:34 +0100

    Added input parser

---
+++ Assets/Scripts/InputParser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputParser : MonoBehaviour
+{
+    public InputField inputField;
+    public GameMaster gameMaster;
+
+   
+
+    public void checkInput(string input)
+    {
+        if(!Regex.IsMatch(input, "[a-z0-9 ]", RegexOptions.IgnoreCase))
+        {
+            Debug.Log("false characters");
+            return;
+        }
+
+        createWords(input);
+    }
+    private void createWords(string input)
+    {
+        List<string> words = new List<string>();
+        string[]  _words = input.Split(' ');
+
+        for (int i = 0; i < _words.Length; i++) {
+            words.Add(_words[i]);
+        }
+
+        gameMaster.sendCommand(words);
+    }
+}
