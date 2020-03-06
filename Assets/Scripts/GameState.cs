6e1bceba83b5ddfc066d6c7677f87b4ce83c8834
mirco <mircobaalmans@live.nl>
2020-03-06 14:32:20 +0100

    created base gamestate

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu(fileName = "GameState", menuName = "Gamestate", order = 0)]
 public class GameState : ScriptableObject
 {
-    
+    public bool playerAlive = true;
+
+    public int questItemsCollected = 0;
+
+    public string CurrentLocation;
+
+    public bool inCombat = false;
+
 }
