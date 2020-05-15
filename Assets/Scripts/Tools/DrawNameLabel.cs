33bea75f40877cccc657f3084b45f84ddcc9f960
mirco <mircobaalmans@live.nl>
2020-05-15 16:50:34 +0200

    Added name label for enemies

---
+++ Assets/Scripts/Tools/DrawNameLabel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawNameLabel : MonoBehaviour
+{
+    [Header("In pixels")]
+    public float heightOffset;
+    [Header("In percentage")]
+    [Range(.1f, 1f)]
+    public float widthOffset = .1f;
+
+    public int fontSize;
+    public int padding;
+
+    public Texture2D labelTexture;
+
+    public bool wordWrap = false;
+
+
+    public Color textColor;
+    [Range(0,1)]
+    public float alpha = 1;
+
+    private void OnGUI()
+    {
+        float objectCenter = this.GetComponent<SpriteRenderer>().bounds.center.x;
+
+        textColor.a = alpha;
+
+        Vector2 playerMiddle = new Vector2(objectCenter, this.transform.position.y);
+        Vector2 pos = Camera.main.WorldToScreenPoint(playerMiddle);
+
+        GUIStyle guiStyle = new GUIStyle();
+        guiStyle.normal.background = labelTexture;
+        guiStyle.fontSize = fontSize;
+        guiStyle.fontStyle = FontStyle.Bold;
+        guiStyle.padding = new RectOffset(padding, padding, padding, padding);
+        guiStyle.wordWrap = wordWrap;
+        guiStyle.normal.textColor = textColor;
+
+        Vector2 textSize = guiStyle.CalcSize(new GUIContent(this.name));
+
+        Rect rect = new Rect(pos.x, Screen.height - pos.y, textSize.x, textSize.y);
+
+        rect.y -= heightOffset;
+        if (this.GetComponent<SpriteRenderer>().flipX) 
+            rect.x -= rect.width * (widthOffset + widthOffset);
+        else
+            rect.x -= rect.width * widthOffset;
+
+        GUI.Box(rect, this.name, guiStyle);
+
+    }
+}
---
+++ Assets/Scripts/Tools/ItemDrop.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour
+{
+    private BoxCollider2D dropField;
+
+    private float width;
+    private float height;
+    private float xoffset;
+    private float yoffset;
+
+    private int amountOfItemsPossibleX = 0;
+    private int amountOfItemsPossibleY = 0;
+    private int _amountOfItemsTotal = 0;
+    private float itemSpriteSizeInUnits = 0;
+    [SerializeField]
+    private List<Vector3> dropPositionsCalculated = new List<Vector3>();
+    [SerializeField]
+    private List<Vector3> dropPositionsAvailable = new List<Vector3>();
+
+    public int amountOfItemsTotal { get => _amountOfItemsTotal; set => _amountOfItemsTotal = value; }
+
+    public void calculateValues()
+    {
+        this.dropField = this.GetComponent<BoxCollider2D>();
+        width = dropField.size.x;
+        height = dropField.size.y;
+
+        xoffset = dropField.offset.x;
+        yoffset = dropField.offset.y;
+
+        itemSpriteSizeInUnits = 1.28f;
+
+        calculateAmount();
+        calculatePositions();
+    }
+
+    private void calculateAmount()
+    {
+        amountOfItemsPossibleX = Mathf.FloorToInt(width / itemSpriteSizeInUnits);
+        amountOfItemsPossibleY = Mathf.FloorToInt(height / itemSpriteSizeInUnits);
+
+        _amountOfItemsTotal = amountOfItemsPossibleX * amountOfItemsPossibleY;
+    }
+
+    private void calculatePositions()
+    {
+        Vector3 startPosition = new Vector3(dropField.bounds.center.x - (dropField.bounds.size.x / 2) + (itemSpriteSizeInUnits / 2), dropField.bounds.center.y - (dropField.bounds.size.y / 2) + (itemSpriteSizeInUnits / 2), 0);
+
+
+        for (int y = 0; y < amountOfItemsPossibleY; y++)
+        {
+            for (int x = 0; x < amountOfItemsPossibleX; x++)
+            {
+
+                dropPositionsCalculated.Add(new Vector3(startPosition.x + (itemSpriteSizeInUnits * x), startPosition.y + (itemSpriteSizeInUnits * y), 0));
+            }
+
+        }
+        dropPositionsAvailable = dropPositionsCalculated;
+    }
+
+
+    public int dropItem(Item item,out ItemOBJ outItemObj)
+    {
+        outItemObj = null;
+        if (item == null) return 0;
+
+
+        int randomIndex = Random.Range(0, dropPositionsAvailable.Count-1);
+
+        ItemOBJ io = Instantiate<ItemOBJ>(Resources.Load<ItemOBJ>("Items/Prefabs/" + item.name), dropPositionsAvailable[randomIndex], Quaternion.identity,this.transform);
+        io.item = item;
+        io.droppedIndex = randomIndex;
+        outItemObj = io;
+        dropPositionsAvailable.RemoveAt(randomIndex);
+
+        return 1;
+    }
+
+    public void itemPickedUp(int i)
+    {
+        if (i == -1) return;
+        dropPositionsAvailable.Add(dropPositionsCalculated[i]);
+    }
+
+
+}
